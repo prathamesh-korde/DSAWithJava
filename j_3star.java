b503a91fbcb338e54fd6aa1a91a9d597a07d3b24
prathamesh-korde <kordeprathamesh01@gmail.com>
2024-10-06 15:58:42 +0530

    Add files via upload

---
+++ j_3star.java
@@ -0,0 +1,10 @@
+
+public class j_3star {
+public static void main(String args[]){
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+}
+}
+        
