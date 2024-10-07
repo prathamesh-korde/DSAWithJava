96d342296da991ce4dbbb5d8355bc74188c64d7f
prathamesh-korde <kordeprathamesh01@gmail.com>
2024-10-07 20:45:14 +0530

    Add files via upload

---
+++ j_7odd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class j_7odd {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd-no");
+        }
+    
+    }
+}
