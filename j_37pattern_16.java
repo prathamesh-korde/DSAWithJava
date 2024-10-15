262aa67db1b65989f7202ab2e4ab6623a602baf4
prathamesh-korde <kordeprathamesh01@gmail.com>
2024-10-15 20:01:02 +0530

    Add files via upload

---
+++ j_37pattern_16.java
@@ -0,0 +1,16 @@
+//pascal's triangle
+import java.util.Scanner;
+public class j_37pattern_16 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int value = 1; 
+            for (int j = 0; j <= i; j++) {
+                System.out.print(value + " ");
+                value = value * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
