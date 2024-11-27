e498117b43165e08f89252f7c6f276a255deaffa
prathamesh-korde <kordeprathamesh01@gmail.com>
2024-11-27 19:53:32 +0530

    Add files via upload

---
+++ J_107.java
@@ -0,0 +1,16 @@
+//implement stack by using existing frameWorks
+import java.util.*;
+public class J_107 {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<Integer>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
